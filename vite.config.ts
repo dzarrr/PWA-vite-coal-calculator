4945298fade1fc306c7f9936d1497cabe7c3e2a8
dzarrr <dzarbelahanifa@gmail.com>
2025-05-24 21:29:41 +0700

    feat: update manifest config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,24 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["apple-icon-180x180.png", "vite.svg"],
-      manifest: false,
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-icon_192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-icon_512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   base: "/PWA-vite-coal-calculator/",
