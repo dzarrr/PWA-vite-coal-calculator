7c1c440ffdc5db3596ad49f18e8784d3f4ae51c5
dzarrr <dzarbelahanifa@gmail.com>
2025-05-24 20:02:45 +0700

    feat: add missing pwa manifest file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["apple-icon-180x180.png", "vite.svg"],
+      manifest: false,
+    }),
+  ],
   base: "/PWA-vite-coal-calculator/",
 });
